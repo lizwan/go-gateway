b3fcf46d614543487403733eee0e4d3434c6302a
zwan <lizwan@gmail.com>
2014-03-26 12:13:08 +0900

    comment, indent

--- listener.js
+++ listener.js
@@ -1,69 +1,78 @@
+// module import
 var MC = require('../../lib/lib').MongoConnection;
 var MQ = require('../../lib/lib').MongoMQ;
 
-var options = {host: 'localhost', databaseName: 'go', queueCollection: 'capped_collection', autoStart: true,
+// mongo db 세팅, capped collection 세팅
+var options = {host: 'localhost', databaseName: 'go', queueCollection: 'mongo_queue', autoStart: true,
     serverOptions: {
-      socketOptions: {
-        connectTimeoutMS: 15000,
-        socketTimeoutMS: 15000
-      }
-    }
+		socketOptions: {
+			connectTimeoutMS: 15000,
+			socketTimeoutMS: 15000
+		}
+	}
 };
-//var options = {servers: ['ndcsrvcdep601', 'ndcsrvcdep602'], databaseName: 'tests', queueCollection: 'capped_collection', autoStart: true};
 
+// mq 변수 설정
 var mq = module.exports = new MQ(options);
-
+var logger = new MC(options);
 var log;
 
+
+// capped collection 테이블에서 데이타 가져와서 지정된 테이블에 저장하기
 var handleRecord = function(err, data, next){
-  if(!err){
 
-    console.log('listener err:'+err+', data:'+JSONtoString(data));
-
-log.insert(data,{w:1},  function(err, details){
-  if(!err){
-     console.log('insert success');
-  } else {
-     console.log('insert fail');
-  }
-});
-
-    next('Hello '+(data||'world')+'!');
-  }else{
-    console.log('err: ', err);
-    next();
-  }
+	//err 가 없으면 target 컬렉션에 insert
+	if(!err){			
+		console.log('listener err:'+err+', data:'+JSONtoString(data));
+		console.log('data.collection:'+data.collection);
+		
+		log.insert(data,{w:1},  function(err, details){
+			if(!err){
+			   console.log('insert success');
+			} else {
+			   console.log('insert fail');
+			}
+		});
+		next(data) );
+	  }else{
+	    console.log('err: ', err);
+	    next();
+	  }
 };
 
+// mq 활성화, server 에서 greet 으로 emit
 mq.on('greet', handleRecord);
 
+// listener 실행될때 target collection 세팅, capped collection start
 (function(){
-  var logger = new MC(options);
-  logger.open(function(err, mc){
-    if(err){
-      console.log('ERROR: ', err);
-    }else{
-      mc.collection('services', function(err, loggingCollection){
-        log = loggingCollection;
-        if(!options.autoStart){
-          mq.start(function(err){
-            if(err){
-              console.log(err);
-            }
-          });
-        }
-      });
-    }
-  });
+	var logger = new MC(options);
+	logger.open(function(err, mc){
+		if(err){
+			console.log('ERROR: ', err);
+		}else{
+			mc.collection('services', function(err, loggingCollection){
+				log = loggingCollection;
+				if(!options.autoStart){
+					mq.start(function(err){
+						if(err){
+							console.log(err);
+						}
+					});
+				}
+			});
+		}
+	});
 })();
 
+// json to string function
 function JSONtoString(object) {
-    var results = [];
-    for (var property in object) {
-        var value = object[property];
-        if (value)
-            results.push(property.toString() + ': ' + value);
-        }
-
-        return '{' + results.join(', ') + '}';
+	var results = [];
+	for (var property in object) {
+		var value = object[property];
+		if (value) {
+			results.push(property.toString() + ': ' + value);
+		}
+	}
+	
+	return '{' + results.join(', ') + '}';
 }
--- server.js
+++ server.js
@@ -1,58 +1,59 @@
+// express server import
 var express = require("express");
-  var app = express();
+var app = express();
+
+// express.bodyParser() 사용
 app.use(express.bodyParser());
 
-
-
+// mongo import
 var MC = require('../../lib/lib').MongoConnection;
 var MQ = require('../../lib/lib').MongoMQ;
 
-var options = {host: 'localhost', databaseName: 'go', queueCollection: 'capped_collection', autoStart: false};
+// mongo option
+var options = {host: 'localhost', databaseName: 'go', queueCollection: 'mongo_queue', autoStart: true};
 
+// mongo 변수 설정
 var mq = module.exports = new MQ(options);
 
-var emitRecord = function(i){
-    console.log('emitting '+i+recordNumber(i,new Date()));
-    mq.emit('greet', recordNumber(i,new Date()), function(err, data){
-        console.log('Response:');
-        console.log(' err>', err);
-        console.log(' dat>', data);
-    });
-};
-
+// mq 준비
 mq.ready(function(){
-    console.log('ready');
+	console.log('ready');
 });
 
+// mongo db connection 맺고
 (function(){
-  var logger = new MC(options);
-  logger.open(function(err, mc){
-    if(err){
-      console.log('ERROR: ', err);
-    }else{
-
-          if(!options.autoStart){
-
-                        mq.start(function(err){
-                          console.log('mq.start');
-                        });
-
-          }
-
-    }
-  });
+	var logger = new MC(options);
+	
+	// connection open
+	logger.open(function(err, mc){
+		if(err){
+			console.log('ERROR: ', err);
+		}else{
+			// mq 작동 시작
+			if(!options.autoStart){
+					mq.start(function(err){
+					console.log('mq.start');
+				});
+			}
+		}
+	});
 })();
 
+// express post request를 받아 처리
 app.post('/', function(req, res){
-  res.send('success');
-  console.log('get in');
-  console.log(req.body);
-
-        mq.emit('greet', req.body, function(err, data){
-                console.log('Response:');
-                console.log(' err>', err);
-                console.log(' dat>', data);
-        });
+  
+	// 클라이언트에게 성공 메세지 보내기
+	res.send('success');
+	
+	console.log('get in');
+	console.log(req.body);
+	
+	// mq 에 emit, put a message on the queue
+	mq.emit('greet', req.body, function(err, data){
+		console.log('Response:');
+		console.log(' err>', err);
+		console.log(' dat>', data);
+	});
 
 
 });
